40c87f1b1b1a9bb19659e5bfdce86f7f8925af94
Haris Tajammal <haris8306827@gmail.com>
2024-11-21 11:13:04 +0500

    Add files via upload

---
+++ Power of x raised to y.cpp
@@ -0,0 +1,12 @@
+#include<math.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x,y;
+	cout<<"Enter value of integer x"<<endl;
+	cin>>x;
+	cout<<"Enter value of integer y"<<endl;
+	cin>>y;
+	cout<<"Power of X raised to Y is "<<pow(x,y);
+}
